e58307046e8d7badb6c2177cf02fdc42c455bd50
Flumenream <moscarafa@hotmail.com>
2019-06-19 19:02:20 +0200

    final update

--- js/script_1.js
+++ js/script_1.js
@@ -1,3 +1,3 @@
 $("#button").click(function(){
-	window.location= "https://github.com/FlumenStream/FlumenStream.github.io/raw/master/flumen.pdf";
+	window.location= "https://drive.google.com/file/d/1y12wbVmcEMTQHen33th2lTJKnPd_abN4/view";
 })
